6d94bc1f3f9db87df9d1463d4e9af698780fa622
vrzhao <coldfusion000@gmail.com>
2018-05-07 18:55:37 +0800

    Added a main page
    Added a main page
    
    bookstore.html probably needs to be changed into a .php
    
    updated the book.xml
    -Need to add image urls
    
    List sorta works, does not filter properly atm
    
    add is still broken

---
+++ list.php
@@ -0,0 +1,35 @@
+<?php
+header("content-type: text/xml");
+
+// Read the XML file into a DOM structure
+$xml = new DOMDocument();
+$xml->preserveWhiteSpace = false; // remove whitespace nodes 
+$xml->load("book.xml");
+
+// Retrieve the GET request values
+$language = $_GET["language"];
+$catagory = $_GET["catagory"];
+
+// Remove the non-matching languages
+if ($language != null) {
+    $languages = $xml->getElementsByTagName("language");
+    for ($i = $languages->count() - 1; $i >= 0; $i--) {
+        $node = $languages->item($i);
+        if ($node->getAttribute("lang") != $language) {
+            $node->parentNode->removeChild($node);
+        }
+    }
+}
+
+// Remove the non-matching catagories
+if ($catagory != null) {
+    $catagories = $xml->getElementsByTagName("book");
+    for ($i = $catagories->count() - 1; $i >= 0; $i--) {
+        $node = $catagories->item($i);
+        if ($node->getAttribute("catagory") != $language) {
+            $node->parentNode->removeChild($node);
+        }
+    }
+}
+echo $xml->saveXML();
+?>
